75ac504f4ab728180a6b0b27761336508eb0d276
rosarie <rosariosanchezsampietro@gmail.com>
2023-05-20 16:27:53 -0300

    Fix typo &apos;

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
     <div className='grid place-content-center mt-5'>
-      <p className='uppercase text-2xl'>Hi! It's Leia</p>
+      <p className='uppercase text-2xl'>Hi! It&apos;s Leia</p>
       <div>
       <a href='https://cafecito.app/leiaceramix' rel='noopener' target='_blank'>
         <div className='w-full h-full'>
