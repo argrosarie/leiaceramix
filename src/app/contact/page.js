616d9858f4376779e498e479093c6849e832857a
rosarie <rosariosanchezsampietro@gmail.com>
2023-06-18 22:04:09 -0300

    Web under construction and spaces y

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -2,7 +2,7 @@ export default function Contact() {
   return (
     <div className="min-h-screen grid place-content-center bg-gradient-to-br from-stone-100 via-stone-300 to-stone-400 text-center">
       <h1 className="uppercase text-slate-900 font-bold text-xl">Contact</h1>
-      <p className="text-slate-900">Sitio en construcción ⚠️</p>
+      <p className="text-slate-900">Web under construction ⚠️</p>
     </div>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ import Instagram from '../../public/assets/icons/instagram.png'
 export default function Home() {
   return (
     <>
-      <div className="flex flex-col md:flex-row justify-center items-center min-h-screen bg-gradient-to-br from-stone-100 via-stone-300 to-stone-400 space-y-8 md:space-x-20 py-20">
+      <div className="flex flex-col md:flex-row justify-center items-center min-h-screen bg-gradient-to-br from-stone-100 via-stone-300 to-stone-400 space-y-10 md:space-x-20 py-24 md:py-20">
         <section className="">
           <div className="">
             <Image
@@ -17,23 +17,26 @@ export default function Home() {
             />
           </div>
         </section>
-        <div className="space-y-4 pb-4 md:space-y-8">
-          <section className="flex flex-col items-center justify-center w-full space-y-2">
+        <div className="space-y-10 pb-4 md:space-y-8">
+          <section className="flex flex-col items-center justify-center w-full space-y-4">
             <p className="uppercase text-3xl text-gray-500 font-bold text-center">
               Hi! <br />
               It&apos;s Leia Ceramix
             </p>
-            <a href="https://instagram.com/leia_ceramix" target="_blank">
-              <Image
-                src={Instagram}
-                width={30}
-                height={30}
-                alt="Instagram icon"
-              />
-            </a>
+            <div className="flex space-x-4 items-center">
+              <p className="text-gray-700 text-lg">Follow me</p>
+              <a href="https://instagram.com/leia_ceramix" target="_blank">
+                <Image
+                  src={Instagram}
+                  width={30}
+                  height={30}
+                  alt="Instagram icon"
+                />
+              </a>
+            </div>
           </section>
 
-          <section className="flex flex-col items-center space-y-1">
+          <section className="flex flex-col items-center space-y-8">
             <a
               href="https://cafecito.app/leiaceramix"
               rel="noopener"
@@ -55,7 +58,7 @@ export default function Home() {
               />
             </a>
             <p className="text-base text-slate-500 font-bold">
-              Sitio en construcción ⚠️
+              Web under construction ⚠️
             </p>
           </section>
         </div>
