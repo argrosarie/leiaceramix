d17165ec5f60f550e34065cee00c7ff774bd9708
rosarie <rosariosanchezsampietro@gmail.com>
2023-06-18 21:52:48 -0300

    Font raleway

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,9 @@
 import Navbar from './components/Navbar/Navbar.js'
 
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Raleway } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const raleway = Raleway({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Leia Ceramix',
@@ -14,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={raleway.className}>
         <Navbar />
         {children}
       </body>
